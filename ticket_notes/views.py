c534558c6a4e2dff66ceb31426470e07659e9895
Dallas LaComb <dallaslacombdrive@gmail.com>
2023-12-12 11:59:31 -0500

    base.html template setup
    Currently the base.html boiler plate template is setup.

--- ticket_notes/views.py
+++ ticket_notes/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
 
 def homepage_view(request):
-    # Your logic here
     return render(request, 'index.html')
 
 def notes_view(request):
-    # Your logic here
     return render(request, 'notes.html')
 
 
-# https://app.ticketmaster.com/discovery/v2/events.json?apikey=D7vEv0uy1A4ZVSByA6KbKX6Yk0EVAQG3&classificationName=${genreArtistEvent}&city=${city}&sort=date,asc
 
