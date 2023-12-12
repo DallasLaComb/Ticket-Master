23db9a43010c54e77b7dac0cbb37e766c16f3589
Dallas LaComb <dallaslacombdrive@gmail.com>
2023-12-12 11:45:12 -0500

    MVT Configuration
    The Model view template configuration is setup how I need it now as far as configuration / pathing. still no content, still no templates at this point.

--- ticket_notes/views.py
+++ ticket_notes/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def homepage_view(request):
+    # Your logic here
+    return render(request, 'index.html')
+
+def notes_view(request):
+    # Your logic here
+    return render(request, 'notes.html')
+
+
+# https://app.ticketmaster.com/discovery/v2/events.json?apikey=D7vEv0uy1A4ZVSByA6KbKX6Yk0EVAQG3&classificationName=${genreArtistEvent}&city=${city}&sort=date,asc
+
