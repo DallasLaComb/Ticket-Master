0a74dae16159f866146a9cab9b5546f356f452aa
Dallas LaComb <dallaslacombdrive@gmail.com>
2023-12-13 11:17:30 -0500

    Update image_url field in BookmarkedEvent model

--- ticket_notes/models.py
+++ ticket_notes/models.py
@@ -8,7 +8,7 @@ class BookmarkedEvent(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     postal_code = models.CharField(max_length=20)
-    image_url = models.URLField(max_length=1024, blank=True, null=True)  # Field for storing image URL
+    image_url = models.URLField(max_length=1024, blank=True, null=True) 
     note = models.TextField(blank=True, null=True)
 
     def __str__(self):
